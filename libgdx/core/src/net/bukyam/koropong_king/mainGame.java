c3518b64198f5557e50dc4e62911e2ad742d276a
janab <brindabatijana@yahoo.com>
2017-11-22 13:19:23 -0800

    eee

--- libgdx/core/src/net/bukyam/koropong_king/mainGame.java
+++ libgdx/core/src/net/bukyam/koropong_king/mainGame.java
@@ -5,19 +5,25 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class mainGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	BitmapFont font;
-	Texture img;
-	int x,y;
+	private SpriteBatch batch;
+	private TextureAtlas textureAtlas;
+	private Sprite sprite;
+	private BitmapFont font;
+	private Texture img;
+	private int x, y, currentFrame;
+	private String currentAtlasKey;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
-		img = new Texture("image.axd.png");
+		//img = new Texture("image.axd.png");
+		textureAtlas = new TextureAtlas(Gdx.files.internal("data/spritesheet.atlas"));
 	}
 
 	@Override
